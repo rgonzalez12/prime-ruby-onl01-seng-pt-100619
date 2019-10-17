a0390c9373096358964aef5e8d48ff9933d12d5d
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-17 04:29:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,4 @@
 def prime?(num)
- return false if integer < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
-            return false
-        end
-    end
-    true
+ (2..integer - 1).each {|x| return false if (integer % x) == 0 }
+  true
 end
