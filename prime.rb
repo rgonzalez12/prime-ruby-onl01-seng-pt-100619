af63c82790d2cd0de7740b19fda3369d79f15e0a
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-17 04:24:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+ result = true 
+   for i in 2..num - 1
+    if num % i == 0
+      result = false
+     break
+   else 
+     false
+   end
+  end
+  return result
+end
